dbd9f20d3eced286b91abe104fe5b3f82eb236dc
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-08 22:00:12 +0530

    added array.c

--- array.c
+++ array.c
@@ -11,3 +11,29 @@ Array_ptr map(Array_ptr src, Mapper mapper)
   }
   return result;
 }
+
+Array_ptr filter(Array_ptr src, Predicate predicate)
+{
+  Array_ptr result = malloc(sizeof(Array_ptr));
+  result->array = malloc(src->length *sizeof(int));
+  int count = 0;
+  for (int i = 0; i < src->length; i++)
+  {
+    if ((*predicate)(src->array[i]))
+    {
+    result->array[count++] = src->array[i];
+    }
+  }
+  result->length = count;
+  return result;
+}
+
+int reduce(Array_ptr src, int init, Reducer reducer)
+{
+  int result = init;
+  for (int i = 0; i < src->length; i++)
+  {
+    result = (*reducer)(result,src->array[i]);
+  }
+  return result;
+}
