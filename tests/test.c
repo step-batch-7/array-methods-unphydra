a78ed83f48e7ecd95358f5321e35a2ecbffaf238
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-08 20:46:38 +0530

    added show_message and show_length_failure functions in test.c

--- tests/test.c
+++ tests/test.c
@@ -1,1 +1,26 @@
 #include "../array.h"
+
+typedef char * String;
+typedef enum
+{
+  Failure,
+  Success
+} Status;
+
+int pass = 0;
+int fail = 0;
+
+Status compare_array(Array,Array);
+void show_message(String,Status);
+void show_length_failure(int, int);
+
+void show_message(String msg, Status s)
+{
+  s?pass++:fail++;
+  printf("%s => %s\n",msg,s?"pass":"fails");
+}
+
+void show_length_failure(int a, int b)
+{
+  printf("\nactual=> %d\nexpected=>%d\n",a,b);
+}
