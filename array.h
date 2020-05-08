6370cce46b4aca663aee3dbe45df296e29aac2e2
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-08 20:35:29 +0530

    added test.c in tests directory, array.c and numbre.c

--- array.h
+++ array.h
@@ -1,11 +1,15 @@
 #ifndef __ARRAY_H
 #define __ARRAY_H
+
+#include <stdio.h>
+#include <stdlib.h>
 typedef enum
 {
   False,
   True
 } Bool;
 
+typedef Array * Array_ptr;
 typedef int (*Mapper)(int);
 typedef Bool (*Predicate)(int);
 typedef int (*Reducer)(int, int);
@@ -16,8 +20,8 @@ typedef struct
   int length;
 } Array;
 
-Array *map(Array *src, Mapper mapper);
-Array *filter(Array *src, Predicate predicate);
-int reduce(Array *src, int init, Reducer reducer);
+Array_ptr map(Array_ptr src, Mapper mapper);
+Array_ptr filter(Array_ptr src, Predicate predicate);
+int reduce(Array_ptr src, int init, Reducer reducer);
 
 #endif
