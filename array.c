af32c3e378265c4d25975123ccbbbdf38d907e85
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-08 21:30:03 +0530

    added map function and test for map function

--- array.c
+++ array.c
@@ -1,2 +1,13 @@
 #include "array.h"
 
+Array_ptr map(Array_ptr src, Mapper mapper)
+{
+  Array_ptr result = malloc(sizeof(Array));
+  result->array = malloc(src->length*sizeof(int));
+  result->length = src->length ? src->length:0;
+  for (int i = 0; i < src->length; i++)
+  {
+    result->array[i] = (*mapper)(src->array[i]);
+  }
+  return result;
+}
--- tests/map_filter_reduce_test.c
+++ tests/map_filter_reduce_test.c
@@ -1,1 +1,28 @@
 #include "test.h"
+
+int test_mapper(int a)
+{
+  return a+1;
+}
+
+void test_map(void)
+{
+  printf("test map\n");
+  Array input;
+  int ar[] = {1,2,3};
+  input.array = ar;
+  input.length = 3;
+  Array_ptr actual = map(&input,test_mapper);
+  Array expected;
+  int exp_ar[] ={2,3,4};
+  expected.array = exp_ar;
+  expected.length = 3;
+  Status s = compare_array(*actual,expected);
+  show_message("should map increment each value in array",s);
+}
+
+int main(void)
+{
+  test_map();
+  return 0;
+}
