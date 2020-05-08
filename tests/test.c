8528b0ac33d0dc9e662101f7c01f9e3e506a8574
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-08 20:53:22 +0530

    added compare_array and compare_value to check two array are equal or not functions in test.c

--- tests/test.c
+++ tests/test.c
@@ -24,3 +24,28 @@ void show_length_failure(int a, int b)
 {
   printf("\nactual=> %d\nexpected=>%d\n",a,b);
 }
+
+Status compare_array(Array actual,Array expected)
+{
+  if (actual.length != expected.length)
+  {
+    show_length_failure(actual.length,expected.length);
+    return Failure;
+  }
+  Status s = Success;
+  for (int i = 0; i < expected.length; i++)
+  {
+    s =s && actual.array[i] == expected.array[i];
+  }
+  return s;
+}
+
+Status compare_value(int a,int e)
+{
+  if (a != e)
+  {
+    show_length_failure(a,e);
+    return Failure;
+  }
+  return Success;
+}
